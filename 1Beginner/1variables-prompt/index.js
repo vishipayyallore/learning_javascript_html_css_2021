a9788568ae20642f1803dfb1e4ce88e0fe6fff2c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-05-14 00:18:57 +0530

    today's work

---
+++ 1Beginner/1variables-prompt/index.js
@@ -0,0 +1,10 @@
+const first = prompt('Enter your first name: ');
+const last = prompt('Enter your last name: ');
+
+alert(`Hello ${first} ${last}`);
+
+const value1 = prompt('Enter first number: ');
+const value2 = prompt('Enter second number: ');
+alert(`Sum: ${+value1 + +value2}`);
+
+// Number | String | Boolean | Undefined | Null | Symbol | Object
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # learning_javascript_html_css_2020
 I am learning JavaScript, HTML, CSS from different website and books
+
+## Data Types
+> 1. Number
+> 1. String
+> 1. Boolean
+> 1. Undefined
+> 1. Null
+> 1. Symbol
+> 1. Object
+
